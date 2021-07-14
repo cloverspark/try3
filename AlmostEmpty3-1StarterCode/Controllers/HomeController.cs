708e594aea994160925c0dde3820b62e400d9bd0
cloverspark <alexbrenna1999@hotmail.com>
2021-07-13 19:03:00 -0700

    working pages and basic set up

---
+++ AlmostEmpty3-1StarterCode/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlmostEmpty3_1StarterCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.title = "Home";
+            
+            return View();
+        }
+        public IActionResult About() 
+        {
+            ViewBag.title = "About";
+            
+            return View();
+        }
+
+        public IActionResult ThankYou()
+        {
+            ViewBag.title = "ThankYou";
+            
+            return View();
+        }
+    }
+}
